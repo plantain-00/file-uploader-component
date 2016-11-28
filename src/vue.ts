995b85d75010eb33061f580d15ce0bcc77969ba5
york yao <yaoao12306@outlook.com>
2016-11-28 22:16:56 +0800

    use vue-class-component

--- src/vue.ts
+++ src/vue.ts
@@ -1,51 +1,45 @@
 import * as Vue from "vue";
 import * as common from "./common";
+import Component from "vue-class-component";
 
-/* tslint:disable:only-arrow-functions */
-/* tslint:disable:no-unused-new */
-/* tslint:disable:object-literal-shorthand */
-
-Vue.component("file-uploader", {
-    template: `
-    <div @drop="onDrop(arguments[0])"
-        @paste="onPaste(arguments[0])"
-        contenteditable="true">
-        <p :style="containerStyle">
-            {{localeObject.dragAndDrop}}
-            <span :style="selectThemStyle">{{localeObject.selectFile}}</span>
-            {{localeObject.pasteFromClipboard}}
-            <input type="file"
-                :style="fileInputStyle"
-                :multiple="multiple"
-                :accept="accept"
-                @change="onFileUploaded(arguments[0])" />
-        </p>
-    </div>
-    `,
+@Component({
     props: ["accept", "multiple", "locale"],
-    data: function (this: This) {
-        const localeObject = common.getLocale(this.locale);
-        return {
-            localeObject,
-            containerStyle: common.containerStyle,
-            selectThemStyle: common.selectThemStyle,
-            fileInputStyle: common.fileInputStyle,
-        };
-    },
-    methods: {
-        onDrop: function (this: This, e: DragEvent) {
-            common.onDrop(file => this.$emit("file-uploaded", file))(e);
-        },
-        onPaste: function (this: This, e: ClipboardEvent) {
-            common.onPaste(file => this.$emit("file-uploaded", file))(e);
-        },
-        onFileUploaded: function (this: This, e: Event) {
-            common.onFileUploaded(file => this.$emit("file-uploaded", file))(e);
-        },
-    },
-});
+    template: `
+        <div @drop="onDrop(arguments[0])"
+            @paste="onPaste(arguments[0])"
+            contenteditable="true">
+            <p :style="containerStyle">
+                {{localeObject.dragAndDrop}}
+                <span :style="selectThemStyle">{{localeObject.selectFile}}</span>
+                {{localeObject.pasteFromClipboard}}
+                <input type="file"
+                    :style="fileInputStyle"
+                    :multiple="multiple"
+                    :accept="accept"
+                    @change="onFileUploaded(arguments[0])" />
+            </p>
+        </div>
+    `,
+})
+class FileUploader extends Vue {
+    accept: string;
+    multiple: boolean;
+    locale: string;
 
-export type This = {
-    $emit: (event: string, args: File | Blob) => void;
-    locale: common.Locale;
-};
+    localeObject = common.getLocale(this.locale);
+    containerStyle = common.containerStyle;
+    selectThemStyle = common.selectThemStyle;
+    fileInputStyle = common.fileInputStyle;
+
+    onDrop(e: DragEvent) {
+        common.onDrop(file => this.$emit("file-uploaded", file))(e);
+    }
+    onPaste(e: ClipboardEvent) {
+        common.onPaste(file => this.$emit("file-uploaded", file))(e);
+    }
+    onFileUploaded(e: Event) {
+        common.onFileUploaded(file => this.$emit("file-uploaded", file))(e);
+    }
+}
+
+Vue.component("file-uploader", FileUploader);
