5ba21750a943ae9689783ca9f30c177b133bee6e
york yao <yaoao12306@outlook.com>
2017-06-30 22:10:21 +0800

    lint config js file

--- demo/rev-static.config.js
+++ demo/rev-static.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    inputFiles: [
-        "demo/**/index.bundle.js",
-        "demo/**/index.ejs.html",
-        "demo/*.bundle.css",
-        "demo/demo.png",
-    ],
-    outputFiles: file => file.replace(".ejs", ""),
-    json: false,
-    ejsOptions: {
-        rmWhitespace: true
-    },
-    sha: 256,
-    customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + "-" + md5String + extensionName,
-};
+  inputFiles: [
+    'demo/**/index.bundle.js',
+    'demo/**/index.ejs.html',
+    'demo/*.bundle.css',
+    'demo/demo.png'
+  ],
+  outputFiles: file => file.replace('.ejs', ''),
+  json: false,
+  ejsOptions: {
+    rmWhitespace: true
+  },
+  sha: 256,
+  customNewFileName: (filePath, fileString, md5String, baseName, extensionName) => baseName + '-' + md5String + extensionName
+}
