dec8139857bf3ba83da50dd91a64c490d2541944
york yao <yaoao12306@outlook.com>
2017-08-21 09:41:54 +0800

    improve watch script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -6,15 +6,19 @@ module.exports = {
     `mkdirp dist`,
     {
       js: [
-        `file2variable-cli src/vue.template.html -o src/vue-variables.ts --html-minify --base src`,
-        `file2variable-cli src/angular.template.html -o src/angular-variables.ts --html-minify --base src`,
+        {
+          vue: `file2variable-cli src/vue.template.html -o src/vue-variables.ts --html-minify --base src`,
+          angular: `file2variable-cli src/angular.template.html -o src/angular-variables.ts --html-minify --base src`
+        },
         `ngc -p src`,
         `tsc -p demo`,
         `webpack --display-modules --config demo/webpack.config.js`
       ],
       css: [
-        `lessc src/file-uploader.less > dist/file-uploader.css`,
-        `cleancss -o dist/file-uploader.min.css dist/file-uploader.css`,
+        [
+          `lessc src/file-uploader.less > dist/file-uploader.css`,
+          `cleancss -o dist/file-uploader.min.css dist/file-uploader.css`
+        ],
         `cleancss -o demo/index.bundle.css dist/file-uploader.min.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
       ],
       clean: `rimraf demo/**/index.bundle-*.js demo/index.bundle-*.css demo/demo-*.png demo/**/*.index.bundle-*.js`,
@@ -51,5 +55,13 @@ module.exports = {
     less: `stylelint --fix "src/*.less"`
   },
   release: `clean-release`,
-  watch: `watch-then-execute "src/**/*.tsx" "demo/**/*.tsx" "src/**/*.ts" "demo/**/*.ts" "spec/**/*.ts" "src/**/*.template.html" --exclude "src/compiled/**/*,src/*-variables.ts" --script "npm run build"`
+  watch: {
+    vue: `file2variable-cli src/vue.template.html -o src/vue-variables.ts --html-minify --base src --watch`,
+    angular: `file2variable-cli src/angular.template.html -o src/angular-variables.ts --html-minify --base src --watch`,
+    src: `tsc -p src --watch`,
+    demo: `tsc -p demo --watch`,
+    webpack: `webpack --config demo/webpack.config.js --watch`,
+    less: `watch-then-execute "src/file-uploader.less" --script "clean-scripts build[2].css[0]"`,
+    rev: `rev-static --config demo/rev-static.config.js --watch`
+  }
 }
