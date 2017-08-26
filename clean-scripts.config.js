eb6e78ec98e5f43d50963020e75e9668279410e7
york yao <yaoao12306@outlook.com>
2017-08-26 16:15:10 +0800

    add screenshot script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -22,7 +22,21 @@ module.exports = {
       clean: `rimraf demo/**/index.bundle-*.js demo/index.bundle-*.css demo/demo-*.png demo/**/*.index.bundle-*.js`,
       cpy: `cpy src/lib.d.ts dist`
     },
-    `rev-static --config demo/rev-static.config.js`
+    `rev-static --config demo/rev-static.config.js`,
+    async () => {
+      const { createServer } = require('http-server')
+      const puppeteer = require('puppeteer')
+      const server = createServer()
+      server.listen(8000)
+      const browser = await puppeteer.launch()
+      const page = await browser.newPage()
+      for (const type of ['vue', 'react', 'angular']) {
+        await page.goto(`http://localhost:8000/demo/${type}`)
+        await page.screenshot({ path: `demo/${type}/screenshot.png`, fullPage: true })
+      }
+      server.close()
+      browser.close()
+    }
   ],
   lint: {
     ts: `tslint "src/**/*.ts" "src/**/*.tsx" "spec/**/*.ts" "demo/**/*.ts" "demo/**/*.tsx"`,
@@ -33,6 +47,9 @@ module.exports = {
   test: [
     'tsc -p spec',
     process.env.APPVEYOR ? 'echo "skip karma test"' : 'karma start spec/karma.config.js',
+    'git checkout demo/vue/screenshot.png',
+    'git checkout demo/react/screenshot.png',
+    'git checkout demo/angular/screenshot.png',
     () => new Promise((resolve, reject) => {
       childProcess.exec('git status -s', (error, stdout, stderr) => {
         if (error) {
