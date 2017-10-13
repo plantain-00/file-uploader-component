599410b75183c608fd413ebf154035d1127dcbf4
york yao <yaoao12306@outlook.com>
2017-10-13 11:05:09 +0800

    refactor lint script

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -4,6 +4,10 @@ const { Service } = require('clean-scripts')
 
 const execAsync = util.promisify(childProcess.exec)
 
+const tsFiles = `"src/**/*.ts" "src/**/*.tsx" "spec/**/*.ts" "demo/**/*.ts" "demo/**/*.tsx" "screenshots/**/*.ts"`
+const lessFiles = `"src/**/*.less"`
+const jsFiles = `"*.config.js"`
+
 module.exports = {
   build: [
     `rimraf dist`,
@@ -30,10 +34,10 @@ module.exports = {
     `rev-static --config demo/rev-static.config.js`
   ],
   lint: {
-    ts: `tslint "src/**/*.ts" "src/**/*.tsx" "spec/**/*.ts" "demo/**/*.ts" "demo/**/*.tsx"`,
-    js: `standard "**/*.config.js"`,
-    less: `stylelint "src/*.less"`,
-    export: `no-unused-export "src/**/*.ts" "src/**/*.tsx" "spec/**/*.ts" "demo/**/*.ts" "demo/**/*.tsx" --exclude "src/compiled/**/*"`
+    ts: `tslint ${tsFiles}`,
+    js: `standard ${jsFiles}`,
+    less: `stylelint ${lessFiles}`,
+    export: `no-unused-export ${tsFiles} ${lessFiles} --exclude "src/compiled/**/*"`
   },
   test: [
     'tsc -p spec',
@@ -47,9 +51,9 @@ module.exports = {
     }
   ],
   fix: {
-    ts: `tslint --fix "src/**/*.tsx" "demo/**/*.tsx" "src/**/*.ts" "demo/**/*.ts" "spec/**/*.ts"`,
-    js: `standard --fix "**/*.config.js"`,
-    less: `stylelint --fix "src/*.less"`
+    ts: `tslint --fix ${tsFiles}`,
+    js: `standard --fix ${jsFiles}`,
+    less: `stylelint --fix ${lessFiles}`
   },
   release: `clean-release`,
   watch: {
