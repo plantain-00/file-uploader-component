41c135cb7ac704da9e731c5ad04301bd4c8b4677
york yao <yaoao12306@outlook.com>
2017-11-15 09:19:40 +0800

    update dependencies

--- demo/react/0.index.bundle.js
+++ demo/react/0.index.bundle.js
@@ -1,1 +1,1 @@
-webpackJsonp([0],{21:function(e,r,o){"use strict";Object.defineProperty(r,"__esModule",{value:!0}),o.d(r,"locale",function(){return t});var t={dragAndDrop:"通过拖放、",selectFile:"选择",pasteFromClipboard:"、或者从剪切板粘贴来上传文件。"}}});
+webpackJsonp([0],{20:function(e,r,o){"use strict";Object.defineProperty(r,"__esModule",{value:!0}),o.d(r,"locale",function(){return t});var t={dragAndDrop:"通过拖放、",selectFile:"选择",pasteFromClipboard:"、或者从剪切板粘贴来上传文件。"}}});
