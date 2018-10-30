0d53a81ff4ca292c75a6e6e45fffcdf091c11aa9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-10-30 15:43:45 +0000

    Deploy plantain-00/file-uploader-component to github.com/plantain-00/file-uploader-component.git:gh-pages

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -75,7 +75,8 @@ module.exports = {
     less: `stylelint ${lessFiles}`,
     export: `no-unused-export ${tsFiles} ${lessFiles} --exclude ${excludeTsFiles}`,
     commit: `commitlint --from=HEAD~1`,
-    markdown: `markdownlint README.md`
+    markdown: `markdownlint README.md`,
+    typeCoverage: 'lerna exec -- type-coverage -p src --at-least 91'
   },
   test: [
     'tsc -p spec',
