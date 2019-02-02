8d3f2b324cbd30335c403e0aa80e5c32a2a43f02
york yao <yaoao12306@outlook.com>
2019-02-02 08:52:16 +0800

    chore: update dependencies

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -76,7 +76,7 @@ module.exports = {
     export: `no-unused-export ${tsFiles} ${lessFiles} --exclude ${excludeTsFiles}`,
     commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
-    typeCoverage: 'lerna exec -- type-coverage -p src'
+    typeCoverage: 'lerna exec -- type-coverage -p src --strict'
   },
   test: [
     'tsc -p spec',
