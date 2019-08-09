75d8e02b5dc11ab91e2a5959c0d3b88770748ca6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-09 23:13:59 +0000

    Deploy plantain-00/file-uploader-component to github.com/plantain-00/file-uploader-component.git:gh-pages

--- packages/react/demo/1.index.bundle.js
+++ packages/react/demo/1.index.bundle.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{10:function(n,o,e){"use strict";e.r(o),e.d(o,"locale",function(){return r});var r={dragAndDrop:"通过拖放、",selectFile:"选择",pasteFromClipboard:"、或者从剪切板粘贴来上传文件。"}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{9:function(n,o,e){"use strict";e.r(o),e.d(o,"locale",function(){return r});var r={dragAndDrop:"通过拖放、",selectFile:"选择",pasteFromClipboard:"、或者从剪切板粘贴来上传文件。"}}}]);
