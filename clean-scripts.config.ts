e72f842fbe890e161020f5158c9d95e35fff8cab
York Yao <me@yorkyao.com>
2020-06-26 13:47:51 +0800

    chore: stop lint js files

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -3,7 +3,6 @@ import { watch } from 'watch-then-execute'
 
 const tsFiles = `"packages/@(core|vue|react)/@(src|demo)/**/*.@(ts|tsx)"`
 const lessFiles = `"packages/core/src/**/*.less"`
-const jsFiles = `"*.config.js"`
 const excludeTsFiles = `"packages/@(core|vue|react)/@(src|demo)/**/*.d.ts"`
 
 const vueTemplateCommand = `file2variable-cli --config packages/vue/src/file2variable.config.ts`
@@ -51,7 +50,7 @@ export default {
     revStaticCommand
   ],
   lint: {
-    ts: `eslint --ext .js,.ts ${tsFiles} ${jsFiles}`,
+    ts: `eslint --ext .js,.ts ${tsFiles}`,
     less: `stylelint ${lessFiles}`,
     export: `no-unused-export ${tsFiles} ${lessFiles} --exclude ${excludeTsFiles}`,
     markdown: `markdownlint README.md`,
@@ -59,7 +58,7 @@ export default {
   },
   test: [],
   fix: {
-    ts: `eslint --ext .js,.ts ${tsFiles} ${jsFiles} --fix`,
+    ts: `eslint --ext .js,.ts ${tsFiles} --fix`,
     less: `stylelint --fix ${lessFiles}`
   },
   watch: {
