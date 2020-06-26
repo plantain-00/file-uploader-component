e4bc51d06111624d82759b38b72d705741a8ce70
York Yao <me@yorkyao.com>
2020-06-26 10:21:50 +0800

    chore: remove commitlint

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -54,7 +54,6 @@ export default {
     ts: `eslint --ext .js,.ts ${tsFiles} ${jsFiles}`,
     less: `stylelint ${lessFiles}`,
     export: `no-unused-export ${tsFiles} ${lessFiles} --exclude ${excludeTsFiles}`,
-    commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
     typeCoverage: 'lerna exec -- type-coverage -p src --strict'
   },
